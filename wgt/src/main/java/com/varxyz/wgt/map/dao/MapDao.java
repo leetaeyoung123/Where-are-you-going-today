34c7f09dbc1b6901063c97771a1eac627e63bedd
leetaeyoung123 <wed456@nate.com>
2022-08-09 18:32:28 +0900

    왜 안되냐고 ^^ㅣ발
    왜 안되냐고 ^^ㅣ발

--- wgt/src/main/java/com/varxyz/wgt/map/dao/MapDao.java
+++ wgt/src/main/java/com/varxyz/wgt/map/dao/MapDao.java
@@ -1,5 +1,21 @@
 package com.varxyz.wgt.map.dao;
 
-public class MapDao {
+import java.util.List;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.varxyz.wgt.map.domain.Map;
+
+public class MapDao {
+	private JdbcTemplate jdbcTemplate;
+	
+	public MapDao(DataSource dataSourceConfig) {
+		jdbcTemplate = new JdbcTemplate(dataSourceConfig);
+	}
+	public List<Map> search(String searchName) {
+		String sql = "SELECT * FROM test WHERE searchName = ?";
+		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Map>(Map.class), searchName);
+	}
 }
