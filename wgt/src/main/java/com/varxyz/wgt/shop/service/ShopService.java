983c25e6da556064c33d678c20e27276b1fa115c
workhan0918 <workhan0918@gmail.com>
2022-08-10 17:38:47 +0900

    중간커밋
    중간커밋

--- wgt/src/main/java/com/varxyz/wgt/shop/service/ShopService.java
+++ wgt/src/main/java/com/varxyz/wgt/shop/service/ShopService.java
@@ -1,5 +1,12 @@
 package com.varxyz.wgt.shop.service;
 
+import com.varxyz.wgt.shop.domain.Menu;
+import com.varxyz.wgt.shop.domain.Shop;
+
 public interface ShopService {
+
+	public boolean addShop(Shop shop);
+	
+	public boolean addMenu(Menu menu);
 	
 }
--- wgt/src/main/java/com/varxyz/wgt/shop/service/ShopServiceImpl.java
+++ wgt/src/main/java/com/varxyz/wgt/shop/service/ShopServiceImpl.java
@@ -1,5 +1,20 @@
 package com.varxyz.wgt.shop.service;
 
+import com.varxyz.wgt.shop.domain.Menu;
+import com.varxyz.wgt.shop.domain.Shop;
+
 public class ShopServiceImpl implements ShopService {
+
+	@Override
+	public boolean addShop(Shop shop) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
