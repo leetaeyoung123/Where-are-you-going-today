9adb77b30a3a0fbb2d735b0bb43ba0d67e927c62
workhan0918 <workhan0918@gmail.com>
2022-08-16 17:55:04 +0900

    다음 주소 api 추가
    다음 주소 api 추가

--- wgt/src/main/java/com/varxyz/wgt/shop/domain/Shop.java
+++ wgt/src/main/java/com/varxyz/wgt/shop/domain/Shop.java
@@ -9,7 +9,10 @@ public class Shop {
 	private String businessNumber;
 	private String shopName;
 	private String shopTel;
+	private String shopPostCode;
 	private String shopAddress;
+	private String shopDetailAddress;
+	private String shopExtraAddress;
 	private String shopHours;
 	private String shopTables;
 	private String shopMaxPeoples;
