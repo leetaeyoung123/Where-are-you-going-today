ad6032217b7ce5f80bf64decf675f04bd617167f
workhan0918 <workhan0918@gmail.com>
2022-08-24 17:28:13 +0900

    slq 테스트코드 추가
    slq 테스트코드 추가

--- wgt/src/main/java/com/varxyz/wgt/sql/SHOP.sql
+++ wgt/src/main/java/com/varxyz/wgt/sql/SHOP.sql
@@ -25,6 +25,11 @@ CREATE TABLE menu (
 -- 테스트 코드
 INSERT INTO shop VALUES('123-456-789', 'testShopName', '053-111-123', '1234', '1234', '1234', '1234', '1234', '1', '1', '1234');
 INSERT INTO menu VALUES('123-456-789', 'testMenu', 'testIntro', 1000, '1234');
+INSERT INTO shop VALUES('111-111-111', 'testShopName2', '053-111-123', '1234', '1234', '1234', '1234', '1234', '1', '1', '1234');
+INSERT INTO menu VALUES('111-111-111', 'testMenu2', 'testIntro', 12000, '1234');
+INSERT INTO menu VALUES('111-111-111', 'testMenu2', 'testIntro2', 22000, '1234');
+INSERT INTO shop VALUES('222-222-222', 'testShopName3', '053-111-123', '1234', '1234', '1234', '1234', '1234', '1', '1', '1234');
+INSERT INTO menu VALUES('222-222-222', 'testMenu3', 'testIntro3', 22000, '1234');
 
 DELETE FROM shop ;
 DELETE FROM MENU ;
